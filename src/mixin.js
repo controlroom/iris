a0add57ecac016e7d747bf470ca79c33d56289e9
Kevin Webster <kevin@kevinwebster.me>
2016-04-12 11:59:40 -0700

    migrated mixin code

--- src/mixin.js
+++ src/mixin.js
@@ -3,3 +3,106 @@
  * @module iris/mixin
  * @flow
  */
+
+import { OrderedSet, List, Map } from "immutable"
+
+class MixinResolver {
+  path: List;
+  base: Class<any>;
+
+  constructor(base: Class<any>) {
+    this.base = base
+    this.resolve()
+  }
+
+  /**
+   * resolve
+   *
+   * Builds a resolve path that first finds all dependencies for
+   * mixins, combines based on load path order, then resolves bottom
+   * up to form the final mixin class path.
+   *
+   * @returns {undefined}
+   */
+  resolve(): void {
+    const resolver = (current, coll) => {
+      return current.reduce((coll, klass) => {
+        if (klass.__iris_implemented)
+          return resolver(klass.__iris_implemented, coll.push(klass))
+        return coll.push(klass)
+      }, coll)
+    }
+    const baseDep = this.base.__iris_implemented
+    if (baseDep) {
+      const allDeps = resolver(baseDep, List([this.base]))
+      const cleanDeps = OrderedSet(allDeps.reverse()).toList()
+      this.path = cleanDeps
+    } else {
+      this.path = List([this.base])
+    }
+  }
+
+  buildResolvedClass(): Class<any> {
+    const klass = this.path.reduce((c, mix) => {
+      return mix(c)
+    }, Metal)
+    klass.__iris_implements = ancestors(klass)
+    return klass
+  }
+}
+
+const ancestors = (klass) => {
+  const back = (list, current) => {
+    const p = Object.getPrototypeOf(current)
+    if(p === Metal) {
+      return list.add(p.name)
+    } else {
+      return back(list.add(p.name), p)
+    }
+  }
+
+  return back(OrderedSet([klass.name]), klass)
+}
+
+export class Metal {
+  static __iris_implements: OrderedSet;
+  opts: Map;
+
+  constructor(opts: Map = Map()) {
+    this.opts = opts
+  }
+
+  static isImplemented(iface) {
+    return this.ancestors.has(iface)
+  }
+
+  static get ancestors() {
+    return this.__iris_implements
+  }
+}
+
+
+/**
+ * implement
+ *
+ * @param {function}[classes]
+ * @returns {undefined}
+ */
+const implement: Function = (...classes) => (base) => {
+  let newBase = base.bind({})
+  newBase.__iris_implemented = classes
+  return newBase
+}
+
+/**
+ * build
+ *
+ * @param klass
+ * @returns {undefined}
+ */
+const build: Function = klass => {
+  let resolver = new MixinResolver(klass)
+  return resolver.buildResolvedClass()
+}
+
+export { MixinResolver, build, implement }
--- test/mixinTest.js
+++ test/mixinTest.js
@@ -1,1 +1,57 @@
 import expect from "expect"
+import { MixinResolver, implement, build } from "../src/mixin"
+import { List, OrderedSet } from "immutable"
+
+const M1 = (superclass) => class M1 extends superclass {}
+let M2   = (superclass) => class M2 extends superclass {}
+M2       = implement(M1)(M2)
+let M3   = (superclass) => class M3 extends superclass {}
+M3       = implement(M2, M1)(M3)
+
+describe("MixinResolver", () => {
+  it("gathers correct resolve path", () => {
+    const resolver = new MixinResolver(M3)
+    expect(resolver.path).toEqual(List([M1, M2, M3]))
+  })
+})
+
+const Mixin1 = (superclass) => class extends superclass {
+  get bar() {
+    return this.foo
+  }
+}
+
+let Mixin2 = (superclass) => class extends superclass {
+  setFoo(v) {
+    this.foo = v
+  }
+}
+
+Mixin2 = implement(Mixin1)(Mixin2)
+
+const Empty = (superclass) => class extends superclass { }
+
+describe("builder", () => {
+  it("works", () => {
+    let a = new (build(Mixin2))
+    a.setFoo(12)
+    expect(a.bar).toEqual(12)
+  })
+
+  it("handles empty mixin", () => {
+    let a = new (build(Empty))
+  })
+})
+
+describe("ancestors", () => {
+  it("can show implemented interfaces", () => {
+    const built = build(M3)
+    expect(built.ancestors).toEqual(OrderedSet(["M3", "M2", "M1", "Metal"]))
+  })
+})
+
+describe("isImplemented", () => {
+  it("can query implemented interfaces", () => {
+    expect(build(M3).isImplemented("M1")).toBe(true)
+  })
+})
