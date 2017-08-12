53b993d39bb49e987a2e240bd15cf61ee44566db
Kevin Webster <kwebster@renewfinancial.com>
2017-08-12 11:38:09 -0700

    Added Meta mixin

---
+++ src/Meta.js
@@ -0,0 +1,92 @@
+/**
+ * Allow storing non-domain data (and flags!)
+ *
+ * @module iris/Meta
+ */
+
+import { IAccess }         from "./Access"
+import { IAffect }         from "./Affect"
+import { implement }       from "./mixin"
+import { Set }             from "immutable"
+import { registerReducer } from "./reducer"
+import { META_KEY }        from "./constants"
+
+import { ensureArray, createNSActions } from "./utils"
+
+const {
+  ADD_FLAGS,
+  REMOVE_FLAGS,
+  TOGGLE_FLAGS,
+} = createNSActions("Meta",
+  "ADD_FLAGS", "REMOVE_FLAGS", "TOGGLE_FLAGS"
+)
+
+let IMeta = (superclass) => {
+  const metaPath = (ctx) => {
+    const path = ctx.metaPath ? ensureArray(ctx.metaPath) : []
+    return ctx.appendPath(META_KEY).concat(path)
+  }
+
+  const extractFlags = (ctx, keyPath) => {
+    const flags = ensureArray(keyPath.pop())
+    keyPath     = (keyPath.length == 0) ? ["flags"] : keyPath
+    return [flags, metaPath(ctx).concat(keyPath)]
+  }
+
+  const flagDispatch = (ctx, type, keyPath) => {
+    const [flags, path] = extractFlags(ctx, keyPath)
+    console.log(path)
+    return ctx.dispatch({type, path, flags})
+  }
+
+  class Meta extends superclass {
+    get meta() {
+      return this.traverse(META_KEY)
+    }
+
+    //---  Flagging  -------------------------------------------------------------
+    flag(...keyPath) {
+      flagDispatch(this, ADD_FLAGS, keyPath)
+    }
+
+    unFlag(...keyPath) {
+      flagDispatch(this, REMOVE_FLAGS, keyPath)
+    }
+
+    toggleFlag(...keyPath) {
+      flagDispatch(this, TOGGLE_FLAGS, keyPath)
+    }
+
+    isFlagged(...keyPath) {
+      const [flags, path] = extractFlags(this, keyPath)
+      const flagSet       = this.getInRoot(path)
+      if(!flagSet) return false
+      return flags.every(flag => flagSet.has(flag))
+    }
+  }
+
+  return Meta
+}
+
+IMeta = implement(IAccess, IAffect)(IMeta)
+
+export { IMeta }
+
+const updateSet = (state, path, fn) =>
+  state.updateIn(path, f => fn(f || Set()))
+
+registerReducer({
+  [ADD_FLAGS]: (state, {path, flags}) => {
+    return updateSet(state, path, set => set.concat(flags))
+  },
+  [REMOVE_FLAGS]: (state, {path, flags}) => {
+    return updateSet(state, path, set => set.subtract(flags))
+  },
+  [TOGGLE_FLAGS]: (state, {path, flags}) => {
+    return updateSet(state, path, set => {
+      const toRemove = set.intersect(flags)
+      const toAdd    = Set(flags).subtract(toRemove)
+      return set.subtract(toRemove).union(toAdd)
+    })
+  }
+})
---
+++ test/MetaTest.js
@@ -0,0 +1,74 @@
+import { expect } from "chai"
+import { IMeta } from "../src/Meta"
+import { build } from "../src/mixin"
+import { Map, List, fromJS } from "immutable"
+import { mockStore } from "./shared"
+
+const Meta = build(IMeta)
+
+const basicMeta = (klass) => {
+  const store = mockStore({})
+  return new klass({store})
+}
+
+describe("Meta", () => {
+  it("can store generic metadata", () => {
+    const o = basicMeta(Meta)
+    o.meta.set("key", "value")
+    expect(o.updateState().meta.get("key")).to.eql("value")
+  })
+
+  describe("Basic Flagging", () => {
+    it("can flag a property", () => {
+      const meta = basicMeta(Meta)
+      meta.flag("fixing")
+      expect(meta.updateState().isFlagged("fixing")).to.be.true
+    })
+
+    it("can unflag a property", () => {
+      const meta = basicMeta(Meta)
+      meta.flag("awesome")
+      expect(meta.updateState().isFlagged("awesome")).to.be.true
+      meta.unFlag("awesome")
+      expect(meta.updateState().isFlagged("awesome")).to.be.false
+    })
+
+    it("can flag multiple tag keys", () => {
+      const meta = basicMeta(Meta)
+      meta.flag(["foo", "bar"])
+      const updatedMeta = meta.updateState()
+      expect(updatedMeta.isFlagged("foo")).to.be.true
+      expect(updatedMeta.isFlagged("bar")).to.be.true
+      expect(updatedMeta.isFlagged("flags")).to.be.false
+    })
+  })
+
+  describe("Flag Groups", () => {
+    it("can create nested tag groups", () => {
+      const meta = basicMeta(Meta)
+      meta.flag("flags", "yes")
+      const updatedMeta = meta.updateState()
+      expect(updatedMeta.isFlagged("flags", "yes")).to.be.true
+      expect(updatedMeta.isFlagged("flags", "boop")).to.be.false
+    })
+
+    it("can accept multiple tag keys", () => {
+      const meta = basicMeta(Meta)
+      meta.flag("flags", ["foo", "bar"])
+      const updatedMeta = meta.updateState()
+      expect(updatedMeta.isFlagged("flags", "foo")).to.be.true
+      expect(updatedMeta.isFlagged("flags", "bar")).to.be.true
+      expect(updatedMeta.isFlagged("flags", ["foo", "bar"])).to.be.true
+      expect(updatedMeta.isFlagged("flags", ["foo", "fun"])).to.be.false
+    })
+
+    it("can toggle tags", () => {
+      const meta = basicMeta(Meta)
+      meta.toggleFlag("basic", ["foo", "bar"])
+      meta.toggleFlag("basic", "foo")
+      const updatedMeta = meta.updateState()
+      expect(updatedMeta.isFlagged("basic", "foo")).to.be.false
+      expect(updatedMeta.isFlagged("basic", "bar")).to.be.true
+    })
+  })
+})
